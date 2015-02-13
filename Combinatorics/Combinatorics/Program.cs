ec501be4a4926a27ea27adefdacfc23443bb2eb8
unknown <dminkov@DMINKOV.telerik.com>
2015-02-13 18:02:36 +0200

    Added algorithms for combinatorics

---
+++ Combinatorics/Combinatorics/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinatorics
+{
+    public static class CombinatoricExtensions
+    {
+        static void Swap<T>(T[] items, int i, int j)
+        {
+            T temp = items[i];
+            items[i] = items[j];
+            items[j] = temp;
+        }
+
+        class PermutationsEquliatyComparer<T> : IEqualityComparer<T[]>
+        {
+            public bool Equals(T[] x, T[] y)
+            {
+                if (x.Length != y.Length)
+                {
+                    return false;
+                }
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (!x[i].Equals(y[i]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            public int GetHashCode(T[] obj)
+            {
+                return obj.Sum(o => o.GetHashCode());
+            }
+        }
+
+        #region Variations
+
+        public static IEnumerable<T[]> GetVariations<T>(this IEnumerable<T> collection, int count)
+        {
+            IList<T[]> variations = new List<T[]>();
+
+            T[] variation = new T[count];
+            T[] items = collection.ToArray();
+            bool[] used = new bool[items.Length];
+
+            GenerateVariations(items, 0, variation, variations, used);
+
+            return variations;
+        }
+
+        private static void GenerateVariations<T>(T[] items, int index, T[] variation, IList<T[]> variations, bool[] used)
+        {
+            if (index == variation.Length)
+            {
+                variations.Add((T[])variation.Clone());
+                return;
+            }
+            
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (!used[i])
+                {
+                    variation[index] = items[i];
+                    used[i] = true;
+                    GenerateVariations(items, index + 1, variation, variations, used);
+                    used[i] = false;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Permutations
+
+        public static IEnumerable<T[]> GetPermutations<T>(this IEnumerable<T> items)
+        {
+            ISet<T[]> permutations = new HashSet<T[]>(new PermutationsEquliatyComparer<T>());
+
+            Permute(items.ToArray(), 0, permutations);
+            return permutations.ToArray();
+        }
+
+        private static void Permute<T>(T[] items, int position, ISet<T[]> permutations)
+        {
+            if (position == items.Length)
+            {
+                permutations.Add((T[])items.Clone());
+                return;
+            }
+            for (int i = position; i < items.Length; i++)
+            {
+                Swap(items, position, i);
+                Permute((T[])items.Clone(), position + 1, permutations);
+                Swap(items, position, i);
+            }
+        }
+
+        #endregion
+
+        #region Combinations
+
+        public static IEnumerable<T[]> GetCombinations<T>(this IEnumerable<T> items, int count)
+        {
+            List<T[]> combinations = new List<T[]>();
+            var combination = new T[count];
+            GetCombinations(items.ToArray(), 0, 0, combination, combinations);
+            return combinations.ToArray();
+        }
+
+        private static void GetCombinations<T>(T[] items, int index, int next, T[] combination, List<T[]> combinations)
+        {
+            if (index == combination.Length)
+            {
+                combinations.Add((T[])combination.Clone());
+                return;
+            }
+            for (int i = next; i < items.Length; i++)
+            {
+                combination[index] = items[i];
+                GetCombinations(items, index + 1, i + 1, combination, combinations);
+            }
+        }
+        #endregion
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Variations:");
+            (new int[] { 1, 2, 3 }).GetVariations(3)
+                                      .ForEach(variation => Console.WriteLine(string.Join(", ", variation)));
+            Console.WriteLine(new string('*', 50));
+
+            //Console.WriteLine("Permutations:");
+            //(new int[] { 1, 2, 3 }).GetPermutations()
+            //                          .ForEach(variation => Console.WriteLine(string.Join(", ", variation)));
+            //Console.WriteLine(new string('*', 50));
+
+            //Console.WriteLine("Combinations:");
+            //(new int[] { 1, 2, 3 }).GetCombinations(2)
+            //                          .ForEach(variation => Console.WriteLine(string.Join(", ", variation)));
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        #region QuickSort
+
+        static T[] QuickSort<T>(T[] numbers) where T : IComparable<T>
+        {
+            var sortedNumbers = numbers.Select(x => x).ToArray();
+            QuickSort(sortedNumbers, 0, sortedNumbers.Length - 1);
+            return sortedNumbers;
+        }
+
+        static void QuickSort<T>(T[] numbers, int start, int end) where T : IComparable<T>
+        {
+            if (start < end)
+            {
+                int partition = Partition(numbers, start, end);
+                QuickSort(numbers, start, partition - 1);
+                QuickSort(numbers, partition + 1, end);
+            }
+        }
+
+        private static int Partition<T>(T[] numbers, int start, int end) where T : IComparable<T>
+        {
+            var pivot = start;
+            var value = numbers[pivot];
+            var temp = numbers[pivot];
+            numbers[pivot] = numbers[end];
+            numbers[end] = temp;
+            var index = start;
+
+            for (int i = start; i <= end; i++)
+            {
+				//if (numbers[i] < value)
+                if (numbers[i].CompareTo(value) < 0)
+                {
+                    temp = numbers[i];
+                    numbers[i] = numbers[index];
+                    numbers[index] = temp;
+                    ++index;
+                }
+            }
+            temp = numbers[index];
+            numbers[index] = numbers[end];
+            numbers[end] = temp;
+            return index;
+        }
+
+        #endregion
+
+        #region MergeSort
+
+        private static T[] MergeSort<T>(T[] numbers) where T : IComparable<T>
+        {
+            var sortedNumbers = numbers.Select(x => x).ToArray();
+
+            T[] tempArray = new T[numbers.Length];
+            MergeSort(sortedNumbers, 0, sortedNumbers.Length - 1, tempArray);
+
+            return sortedNumbers;
+        }
+
+        private static void MergeSort<T>(T[] numbers, int from, int to, T[] tempArray) where T : IComparable<T>
+        {
+            if (from < to)
+            {
+                int middle = (from + to) / 2;
+                MergeSort(numbers, from, middle, tempArray);
+                MergeSort(numbers, middle + 1, to, tempArray);
+                Merge(numbers, from, to, tempArray);
+            }
+        }
+
+        private static void Merge<T>(T[] numbers, int from, int to, T[] tempArray) where T : IComparable<T>
+        {
+            int middle = (from + to) / 2;
+
+            int left = from;
+            int right = middle + 1;
+            int pos = from;
+
+            while ((left <= middle) && (right <= to))
+            {
+                if (numbers[left].CompareTo(numbers[right]) < 0)
+                {
+                    tempArray[pos] = numbers[left];
+                    ++left;
+                }
+                else
+                {
+                    tempArray[pos] = numbers[right];
+                    ++right;
+                }
+                ++pos;
+            }
+
+            while (left <= middle)
+            {
+                tempArray[pos] = numbers[left];
+                ++pos;
+                ++left;
+            }
+            while (right <= to)
+            {
+                tempArray[pos] = numbers[right];
+                ++pos;
+                ++right;
+            }
+
+            for (int i = from; i <= to; i++)
+            {
+                numbers[i] = tempArray[i];
+            }
+        }
+
+        #endregion
+
+        #region BubbleSort
+
+        static T[] BubbleSort<T>(T[] items) where T : IComparable<T>
+        {
+            T[] sortedItems = (T[])items.Clone();
+
+            bool isSwapDone = false;
+            do
+            {
+                isSwapDone = false;
+                for (int j = 0; j < sortedItems.Length - 1; j++)
+                {
+                    if (sortedItems[j].CompareTo(sortedItems[j + 1]) > 0)
+                    {
+                        isSwapDone = true;
+                        var temp = sortedItems[j];
+                        sortedItems[j] = sortedItems[j + 1];
+                        sortedItems[j + 1] = temp;
+                    }
+                }
+            }
+            while (isSwapDone);
+
+            return sortedItems;
+        }
+
+        #endregion
+
+        #region SelectionSort
+
+        static T[] SelectionSort<T>(T[] items) where T : IComparable<T>
+        {
+            var sortedItems = (T[])items.Clone();
+
+            for (int i = 0; i < sortedItems.Length; i++)
+            {
+                for (int j = sortedItems.Length - 1; j > i; j--)
+                {
+                    if (sortedItems[i].CompareTo(sortedItems[j]) > 0)
+                    {
+                        var temp = sortedItems[i];
+                        sortedItems[i] = sortedItems[j];
+                        sortedItems[j] = temp;
+                    }
+                }
+            }
+
+            return sortedItems;
+        }
+
+        #endregion
+
+        static Random rand = new Random();
+
+        static void Main()
+        {
+            int count = 10000;
+
+            int[] numbers = Enumerable.Range(1, count).Reverse().ToArray();
+            //int[] numbers = new int[count];
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    numbers[i] = rand.Next();
+            //}
+
+            var start = DateTime.Now;
+            Console.WriteLine("With built-in sort: ");
+            var sortedNumbersWithBuiltInSort = numbers.OrderBy(number => number);
+            //Console.WriteLine(string.Join(", ", numbers));
+            var end = DateTime.Now;
+            Console.WriteLine("Finished in {0} seconds", end - start);
+
+            start = DateTime.Now;
+            var sortedNumbersWithQuickSort = QuickSort(numbers);
+            Console.WriteLine("With QuickSort: ");
+            //Console.WriteLine(string.Join(", ", sortedNumbersWithQuickSort));
+            end = DateTime.Now;
+            Console.WriteLine("Finished in {0} seconds", end - start);
+
+            start = DateTime.Now;
+            var sortedNumbersWithMergeSort = MergeSort(numbers);
+            Console.WriteLine("With MergeSort: ");
+            //Console.WriteLine(string.Join(", ", sortedNumbersWithMergeSort));
+            end = DateTime.Now;
+            Console.WriteLine("Finished in {0} seconds", end - start);
+
+            start = DateTime.Now;
+            var sortedNumbersWithBubbleSort = BubbleSort(numbers);
+            Console.WriteLine("With BubbleSort: ");
+            //Console.WriteLine(string.Join(", ", sortedNumbersWithBubbleSort));
+            end = DateTime.Now;
+            Console.WriteLine("Finished in {0} seconds", end - start);
+
+            start = DateTime.Now;
+            var sortedNumbersWithSelectionSort = SelectionSort(numbers);
+            Console.WriteLine("With SelectionSort: ");
+            //Console.WriteLine(string.Join(", ", sortedNumbersWithSelectionSort));
+            end = DateTime.Now;
+            Console.WriteLine("Finished in {0} seconds", end - start);
+        }
+    }
+}

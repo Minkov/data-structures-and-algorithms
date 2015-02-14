99df40d566d50aa644bc02603628560de00f028d
Doncho Minkov <donchominkov@gmail.com>
2015-02-14 14:19:08 +0200

    Fixed the formatting

--- Algorithms/SortingAlgorithms/SortingAlgorithms/Program.cs
+++ Algorithms/SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -6,189 +6,172 @@ using System.Threading.Tasks;
 
 namespace SortingAlgorithms
 {
-    class Program
-    {
-		static void Swap<T>(T[] items, int i, int j){
+	class Program
+	{
+		static void Swap<T> (T[] items, int i, int j)
+		{
 			var temp = items [i];
 			items [i] = items [j];
 			items [j] = temp;
 		}
 
-        #region QuickSort
+		#region QuickSort
 
-		static T[] QuickSort<T>(T[] items) where T : IComparable<T>
-        {
-			var toBeSortedItems = items.Select(x => x).ToArray();
-			QuickSort(toBeSortedItems, 0, toBeSortedItems.Length - 1);
+		static T[] QuickSort<T> (T[] items) where T : IComparable<T>
+		{
+			var toBeSortedItems = items.Select (x => x).ToArray ();
+			QuickSort (toBeSortedItems, 0, toBeSortedItems.Length - 1);
 			return toBeSortedItems;
-        }
+		}
 
-        static void QuickSort<T>(T[] items, int start, int end) where T : IComparable<T>
-        {
-            if (start < end)
-            {
-				int partition = Partition(items, start, end);
-				QuickSort(items, start, partition - 1);
-				QuickSort(items, partition + 1, end);
-            }
-        }
+		static void QuickSort<T> (T[] items, int start, int end) where T : IComparable<T>
+		{
+			if (start < end) {
+				int partition = Partition (items, start, end);
+				QuickSort (items, start, partition - 1);
+				QuickSort (items, partition + 1, end);
+			}
+		}
 
-		private static int Partition<T>(T[] items, int start, int end) where T : IComparable<T>
-        {
+		private static int Partition<T> (T[] items, int start, int end) where T : IComparable<T>
+		{
 			var pivot = start;
-			var value = items[pivot];
+			var value = items [pivot];
 		
 			//swap
 			Swap (items, pivot, end);
 
-            var index = start;
-			for (int i = start; i <= end; i++)
-            {
-                if (items[i].CompareTo(value) < 0)
-                {
+			var index = start;
+			for (int i = start; i <= end; i++) {
+				if (items [i].CompareTo (value) < 0) {
 					Swap (items, i, index);
-                    ++index;
-                }
-            }
+					++index;
+				}
+			}
 			Swap (items, index, end);
-            return index;
-        }
+			return index;
+		}
 
-        #endregion
+		#endregion
 
-        #region MergeSort
+		#region MergeSort
 
-        private static T[] MergeSort<T>(T[] numbers) where T : IComparable<T>
-        {
-            var sortedNumbers = numbers.Select(x => x).ToArray();
+		private static T[] MergeSort<T> (T[] numbers) where T : IComparable<T>
+		{
+			var sortedNumbers = numbers.Select (x => x).ToArray ();
 
-            T[] tempArray = new T[numbers.Length];
-            MergeSort(sortedNumbers, 0, sortedNumbers.Length - 1, tempArray);
+			T[] tempArray = new T[numbers.Length];
+			MergeSort (sortedNumbers, 0, sortedNumbers.Length - 1, tempArray);
 
-            return sortedNumbers;
-        }
+			return sortedNumbers;
+		}
 
-        private static void MergeSort<T>(T[] numbers, int from, int to, T[] tempArray) where T : IComparable<T>
-        {
-            if (from < to)
-            {
-                int middle = (from + to) / 2;
-                MergeSort(numbers, from, middle, tempArray);
-                MergeSort(numbers, middle + 1, to, tempArray);
-                Merge(numbers, from, to, tempArray);
-            }
-        }
+		private static void MergeSort<T> (T[] numbers, int from, int to, T[] tempArray) where T : IComparable<T>
+		{
+			if (from < to) {
+				int middle = (from + to) / 2;
+				MergeSort (numbers, from, middle, tempArray);
+				MergeSort (numbers, middle + 1, to, tempArray);
+				Merge (numbers, from, to, tempArray);
+			}
+		}
 
-        private static void Merge<T>(T[] numbers, int from, int to, T[] tempArray) where T : IComparable<T>
-        {
-            int middle = (from + to) / 2;
+		private static void Merge<T> (T[] numbers, int from, int to, T[] tempArray) where T : IComparable<T>
+		{
+			int middle = (from + to) / 2;
 
-            int left = from;
-            int right = middle + 1;
-            int pos = from;
+			int left = from;
+			int right = middle + 1;
+			int pos = from;
 
-            while ((left <= middle) && (right <= to))
-            {
-                if (numbers[left].CompareTo(numbers[right]) < 0)
-                {
-                    tempArray[pos] = numbers[left];
-                    ++left;
-                }
-                else
-                {
-                    tempArray[pos] = numbers[right];
-                    ++right;
-                }
-                ++pos;
-            }
+			while ((left <= middle) && (right <= to)) {
+				if (numbers [left].CompareTo (numbers [right]) < 0) {
+					tempArray [pos] = numbers [left];
+					++left;
+				} else {
+					tempArray [pos] = numbers [right];
+					++right;
+				}
+				++pos;
+			}
 
-            while (left <= middle)
-            {
-                tempArray[pos] = numbers[left];
-                ++pos;
-                ++left;
-            }
-            while (right <= to)
-            {
-                tempArray[pos] = numbers[right];
-                ++pos;
-                ++right;
-            }
+			while (left <= middle) {
+				tempArray [pos] = numbers [left];
+				++pos;
+				++left;
+			}
+			while (right <= to) {
+				tempArray [pos] = numbers [right];
+				++pos;
+				++right;
+			}
 
-            for (int i = from; i <= to; i++)
-            {
-                numbers[i] = tempArray[i];
-            }
-        }
+			for (int i = from; i <= to; i++) {
+				numbers [i] = tempArray [i];
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region BubbleSort
+		#region BubbleSort
 
-        static T[] BubbleSort<T>(T[] items) where T : IComparable<T>
-        {
-            T[] sortedItems = (T[])items.Clone();
+		static T[] BubbleSort<T> (T[] items) where T : IComparable<T>
+		{
+			T[] sortedItems = (T[])items.Clone ();
 
-            bool isSwapDone = false;
-            do
-            {
-                isSwapDone = false;
-                for (int j = 0; j < sortedItems.Length - 1; j++)
-                {
-                    if (sortedItems[j].CompareTo(sortedItems[j + 1]) > 0)
-                    {
-                        isSwapDone = true;
-                        var temp = sortedItems[j];
-                        sortedItems[j] = sortedItems[j + 1];
-                        sortedItems[j + 1] = temp;
-                    }
-                }
-            }
-            while (isSwapDone);
+			bool isSwapDone = false;
+			do {
+				isSwapDone = false;
+				for (int j = 0; j < sortedItems.Length - 1; j++) {
+					if (sortedItems [j].CompareTo (sortedItems [j + 1]) > 0) {
+						isSwapDone = true;
+						var temp = sortedItems [j];
+						sortedItems [j] = sortedItems [j + 1];
+						sortedItems [j + 1] = temp;
+					}
+				}
+			} while (isSwapDone);
 
-            return sortedItems;
-        }
+			return sortedItems;
+		}
 
-        #endregion
+		#endregion
 
-        #region SelectionSort
+		#region SelectionSort
 
-        static T[] SelectionSort<T>(T[] items) where T : IComparable<T>
-        {
-            var sortedItems = (T[])items.Clone();
+		static T[] SelectionSort<T> (T[] items) where T : IComparable<T>
+		{
+			var sortedItems = (T[])items.Clone ();
 
-            for (int i = 0; i < sortedItems.Length; i++)
-            {
-                for (int j = sortedItems.Length - 1; j > i; j--)
-                {
-                    if (sortedItems[i].CompareTo(sortedItems[j]) > 0)
-                    {
-                        var temp = sortedItems[i];
-                        sortedItems[i] = sortedItems[j];
-                        sortedItems[j] = temp;
-                    }
-                }
-            }
+			for (int i = 0; i < sortedItems.Length; i++) {
+				for (int j = sortedItems.Length - 1; j > i; j--) {
+					if (sortedItems [i].CompareTo (sortedItems [j]) > 0) {
+						var temp = sortedItems [i];
+						sortedItems [i] = sortedItems [j];
+						sortedItems [j] = temp;
+					}
+				}
+			}
 
-            return sortedItems;
-        }
+			return sortedItems;
+		}
 
-        #endregion
+		#endregion
 
-        static Random rand = new Random();
+		static Random rand = new Random ();
 
-        static void Main()
-        {
-            int count = 10;
+		static void Main ()
+		{
+			int count = 10;
 			DateTime start, end;
 
-            int[] numbers = Enumerable.Range(1, count).Reverse().ToArray();
-            //int[] numbers = new int[count];
+			int[] numbers = Enumerable.Range (1, count).Reverse ().ToArray ();
+			//int[] numbers = new int[count];
 
-            //for (int i = 0; i < numbers.Length; i++)
-            //{
-            //    numbers[i] = rand.Next();
-            //}
+			//for (int i = 0; i < numbers.Length; i++)
+			//{
+			//    numbers[i] = rand.Next();
+			//}
 
 //            start = DateTime.Now;
 //            Console.WriteLine("With built-in sort: ");
@@ -197,12 +180,12 @@ namespace SortingAlgorithms
 //            end = DateTime.Now;
 //            Console.WriteLine("Finished in {0} seconds", end - start);
 
-            start = DateTime.Now;
-            var sortedNumbersWithQuickSort = QuickSort(numbers);
-            Console.WriteLine("With QuickSort: ");
-            Console.WriteLine(string.Join(", ", sortedNumbersWithQuickSort));
-            end = DateTime.Now;
-            Console.WriteLine("Finished in {0} seconds", end - start);
+			start = DateTime.Now;
+			var sortedNumbersWithQuickSort = QuickSort (numbers);
+			Console.WriteLine ("With QuickSort: ");
+			Console.WriteLine (string.Join (", ", sortedNumbersWithQuickSort));
+			end = DateTime.Now;
+			Console.WriteLine ("Finished in {0} seconds", end - start);
 
 //            start = DateTime.Now;
 //            var sortedNumbersWithMergeSort = MergeSort(numbers);
@@ -224,6 +207,6 @@ namespace SortingAlgorithms
 //            //Console.WriteLine(string.Join(", ", sortedNumbersWithSelectionSort));
 //            end = DateTime.Now;
 //            Console.WriteLine("Finished in {0} seconds", end - start);
-        }
-    }
+		}
+	}
 }
